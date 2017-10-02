d852c48c874dd03e82d71f71b4e7c14504a5ba2e
RBR <rodrobauram@hotmail.com>
2017-10-02 11:11:47 -0500

    creación del proyecto

---
+++ src/main/java/com/va/tool/VaToolApplication.java
@@ -0,0 +1,12 @@
+package com.va.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaToolApplication.class, args);
+	}
+}
